fff983723b93dadd54a358db7991757922d36128
Joe Littlejohn <joe.littlejohn@nokia.com>
2014-08-07 17:22:46 +0100

    Update dependency versions

--- project.clj
+++ project.clj
@@ -2,18 +2,18 @@
 
   :description "A Clojure library that wraps the Curator service discovery/registration API."
 
-  :dependencies [[cheshire "5.2.0"]
-                 [environ "0.4.0"]
+  :dependencies [[cheshire "5.3.1"]
+                 [environ "0.5.0"]
                  [org.flatland/useful "0.11.2"]
-                 [org.apache.curator/curator-x-discovery "2.5.0" :exclusions [org.slf4j/slf4j-log4j12]]
+                 [org.apache.curator/curator-x-discovery "2.6.0" :exclusions [org.slf4j/slf4j-log4j12]]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [zookeeper-clj "0.9.3"]]
 
-  :profiles {:dev {:dependencies [[zookem "0.1.0-SNAPSHOT"]
-                                  [midje "1.5.1"]
-                                  [org.slf4j/log4j-over-slf4j "1.7.6"]
-                                  [org.slf4j/slf4j-simple "1.7.6"]]
+  :profiles {:dev {:dependencies [[zookem "0.1.2"]
+                                  [midje "1.6.3"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.7"]
+                                  [org.slf4j/slf4j-simple "1.7.7"]]
                    :plugins [[lein-midje "3.0.1"]
                              [lein-release "1.0.5"]
                              [codox "0.8.9"]]
